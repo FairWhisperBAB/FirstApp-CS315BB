53762ace22c8278427e619be23b876257ce05779
Benjamin Briggs <bbriggs2002@gmail.com>
2022-09-06 08:15:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firstapp_cs315bb/BeeMovieScript.java
@@ -0,0 +1,19 @@
+package com.example.firstapp_cs315bb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class BeeMovieScript extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bee_movie_script);
+
+    }
+}
